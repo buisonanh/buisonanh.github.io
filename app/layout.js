1d9cc4d47151509827ee341add669f350f378e37
Son Anh <95670014+buisonanh@users.noreply.github.com>
2025-03-03 14:34:26 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         <div className="fixed top-0 left-0 w-full h-full -z-10">
           <div 
             className="w-full h-full bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: "url('/background.png')" }}
+            style={{ backgroundImage: "url('https://images.pexels.com/photos/1341279/pexels-photo-1341279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')" }}
           />
         </div>
 
